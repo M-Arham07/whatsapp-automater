6eb550444b399b7386ec9bc99e2e5f7333f9c381
M-Arham07 <khawajaarham15@gmail.com>
2025-08-22 15:12:18 +0500

    Changed Folder Structure

--- server/whatsapp-server.ts
+++ server/whatsapp-server.ts
@@ -1,17 +1,48 @@
-import { Socket, Server } from "socket.io";
+import { Server } from "socket.io";
+import type {Socket} from "socket.io";
 import { createServer } from "node:http";
 import pkg, { Client } from "whatsapp-web.js";
+import type { Message } from "whatsapp-web.js";
+import fs from "fs";
+
+
+
+// PORT ::
+
+const PORT : number = 8080;
+
+
+// JUST COLORS:::
+const Reset = "\x1b[0m";  // reset to default
+const Bright = "\x1b[1m"; // bright/bold
+const FgRed = "\x1b[31m";
+const FgGreen = "\x1b[32m";
+const FgYellow = "\x1b[33m";
+const FgBlue = "\x1b[34m";
+
+
+
+
 
 const { LocalAuth } = pkg;
 
+const SESSION_FILE_PATH: string = "./session.json"
+
 let client: Client | null = null;
 
-if (!client) {
-    client = new Client({
-        authStrategy: new LocalAuth()
-    });
-    client.initialize();
+let sessionData;
+
+if (fs.existsSync(SESSION_FILE_PATH)){
+    sessionData = require(SESSION_FILE_PATH);
 }
+
+    client = new Client({
+        authStrategy: new LocalAuth({
+            clientId:"123",
+            dataPath:"./sessions"
+        })
+    })
+client?.initialize();
 
 
 const server = createServer();
@@ -27,30 +58,60 @@ io.on("connection", (socket: Socket): void => {
     console.log(">> Connection Established!");
 
 
-    socket.on("qr_request", (): void => {
-
-
-        client.once("qr", (qr_string: string) => {
-
-
-            console.log(">> Generated QR Success, emitting response");
-            socket.emit("qr_response", qr_string);
-
-        });
-
-
-        client.once("ready", (): void => {
-            console.log(">> WA Client Ready!");
-            socket.emit("client_ready");
-
-        });
-
-
+    client.on("authenticated", (): void => {
+        console.log("")
+        socket.emit("auth_success");
 
     })
 
 
 
+
+    socket.on("qr_request", (): void => {
+
+
+
+        // SOME SORT OF IF CLIENT CHECK?
+
+
+        client.on("qr", (qr_string: string) => {
+
+
+            console.log(`>> ${FgBlue} Generated QR Success, emitting response ${Reset}`);
+            socket.emit("qr_response", qr_string);
+
+        })
+
+
+
+        client.on("ready", (): void => {
+            console.log(`>> ${FgGreen} WA Client Ready! ${Reset}`);
+            socket.emit("client_ready");
+
+        });
+
+    });
+
+    socket.on("start_automation", (autoMsg) => {
+
+        console.log(`${FgGreen}::::::Starting Automation:::::: ${Reset}`);
+
+        client.emit("automation_started");
+
+        client.on("message", async (message: Message): Promise<void> => {
+
+            console.log(`${FgGreen} REPLYING WITH ${Reset} ${autoMsg} `);
+            await message.reply(autoMsg);
+            console.log(`${FgYellow} Successfully replied to ${Reset} ${message.from.slice(0, -4)}`);
+
+
+        });
+        console.log(`${FgGreen} Listening for Incoming Messages ${Reset}`);
+
+    });
+
+
+
 });
 
-server.listen(8080,()=>console.log("Websocket started::"))
+server.listen(PORT, () => console.log(`${FgBlue} <---------Websocket started at port ${PORT}---------> ${Reset}`))
